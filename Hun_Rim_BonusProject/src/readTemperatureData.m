ce5b07d2371562c254f63de0ffd935ee1995951b
TKartist <rimh@usi.ch>
2023-11-27 21:59:40 +0100

    finished reading data from temperature table

---
+++ Hun_Rim_BonusProject/src/readTemperatureData.m
@@ -0,0 +1,21 @@
+function [x, y] = readTemperatureData(filepath, s, e)
+    data = readtable(filepath, 'NumHeaderLines', 16);
+    year = data.Var1;
+    temperatures = [data.Var2, data.Var3, data.Var4, data.Var5, data.Var6, data.Var7, data.Var8, data.Var9, data.Var10, data.Var11, data.Var12, data.Var13];
+    x = zeros((e - s) * 12, 1);
+    y = zeros((e - s) * 12, 1);
+    e = e - 1;
+    count = 0;
+    for i = 1:height(data)
+        if year(i) >= s
+            for j = 1 : 12
+                y(count * 12 + j, 1) = temperatures(i, j);
+                x(count * 12 + j, 1) = year(i) + (1 / 12 * (j - 1));
+            end
+            count = count + 1;
+        end
+        if year(i) >= e
+            break
+        end
+    end
+end
